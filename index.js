430b9219cf0d60e0b09516ed99f6e890178ca60e
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 20:36:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = tutorials.map(function(tutorial) => {
-  return tutorials = titleCased.map()
+const titleCased = () => {
+  return tutorials.map(function(tutorial) {
+    tutorial=tutorial.split('');
+    for(let i -0; i < tutorial.length; i++) {
+      tutorial[i]=tutorial[i].charAt(0).toUpperCase()+tutorial[i].slice(1);
+    }
+    return turtorial.join('');
+  });
 }
